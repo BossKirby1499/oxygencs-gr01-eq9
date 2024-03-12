56b619ea06280cf3fe46a363c9676ddd85238c38
gabrielsvg91 <106838616+gabrielsvg91@users.noreply.github.com>
2024-03-12 16:55:20 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,14 +1,12 @@
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.join(current_dir, '../src')
+sys.path.append(src_dir)
 from main import App
 import sys
 import os
 import json
 import unittest
 from unittest.mock import patch, MagicMock
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.join(current_dir, '../src')
-sys.path.append(src_dir)
-
 
 class TestApp(unittest.TestCase):
     # Simule le véritable HubConnectionBuilder du paquet signalrcore
